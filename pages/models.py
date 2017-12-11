204019d713bb2f73229142e284f7ad5ef165ecf4
Paul Crooks <pgcrooks@gmail.com>
2017-12-11 21:32:29 +0000

    Adding blog Post model
    Simple model definiton and the assoicated DB migration.

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Blog post.
+    """
+    post_text = models.CharField(max_length=5000)
+    pub_date = models.DateTimeField('date published')
