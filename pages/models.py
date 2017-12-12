b2b61ef8b4cc74a8131d26eb608e6c7dc5ee5106
Paul Crooks <pgcrooks@gmail.com>
2017-12-12 16:49:01 +0000

    Model update to include more Post fields
    The Post model now includes a title and a pinned tag. Added stringify and latest post methods. This update includes the DB migration and SQLite update.

--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
     """
-    Blog post.
+    A Blog post.
+    Consists of a title, blog text, a publication date and a pinned flag.
+    Pinned posts remain at the top.
     """
-    post_text = models.CharField(max_length=5000)
+    title = models.CharField(max_length=100, default='Title')
+    text = models.CharField(max_length=5000)
     pub_date = models.DateTimeField('date published')
+    pinned = models.BooleanField(default=False)
+
+    def __str__(self):
+        """
+        Stringify a Post
+        """
+        return self.title
+
+    def was_published_recently(self):
+        """
+        Returns True if this post was published recently
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
