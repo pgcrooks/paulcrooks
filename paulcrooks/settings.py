3293cf60ff708f9557d09e4bbdb784fa6851ffe6
Paul Crooks <pgcrooks@gmail.com>
2020-11-18 08:00:42 +0000

    Enable debug from env

--- paulcrooks/settings.py
+++ paulcrooks/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ['DEBUG_ON'] == "TRUE":
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
